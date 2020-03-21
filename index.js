4d7bdd516bf59b05ab2577d0ff6e3d81bf7af4c2
kwasniew <kwasniewski.mateusz@gmail.com>
2020-03-21 08:26:45 +0100

    remove css

--- index.js
+++ index.js
@@ -1,152 +1,153 @@
-import {h, app} from "./web_modules/hyperapp.js";
-import {request} from "./web_modules/@hyperapp/http.js";
+import { h, app } from "./web_modules/hyperapp.js";
+import { request } from "./web_modules/@hyperapp/http.js";
 import htm from "./web_modules/htm.js";
-import {targetValue} from "./web_modules/@hyperapp/events.js";
-import Chart from './web_modules/chart.js/dist/Chart.js';
-import {stringToRGB} from "./stringToColor.js";
+import { targetValue } from "./web_modules/@hyperapp/events.js";
+import Chart from "./web_modules/chart.js/dist/Chart.js";
+import { stringToRGB, stringToHex } from "./stringToColor.js";
+import { countries } from "./countries.js";
 
-// const initialData = {
-//     labels: [
-//         "29/01", "29/01", "29/01", "29/01", "29/01"
-//     ],
-//     datasets: [
-//         {name: "China", type: "line", values: [0,0,0,0,0]},
-//         // {name: "Poland", type: "line", values: [1, 20, 30, 40, 5]},
-//         // {name: "Italy", type: "line", values: [100, 200, 300, 40, 5]},
-//         // {name: "Italy1", type: "line", values: [120, 220, 300, 40, 5]},
-//     ]
-// };
 
 const personify = d => d + (d === 1 ? " person" : " people");
 
-// let chart;
-// const makeApexChart = ({series, categories}) => {
-//     if(chart) {
-//         chart.updateSeries(series);
-//         return;
-//     }
-//     const newChart = new ApexCharts(document.querySelector('#chart'), {
-//         chart: {
-//             type: 'line'
-//         },
-//         series,
-//         xaxis: {
-//             categories
-//         }
-//     });
-//     newChart.render();
-//     return newChart;
-// };
-
-// const makeChart = (data) => new Chart("#chart", { // or DOM element
-//     data,
-//     title: "Coronavirus Confirmed Cases",
-//     type: 'line', // or 'bar', 'line', 'pie', 'percentage'
-//     height: 800,
-//
-//     tooltipOptions: {
-//         formatTooltipX: d => d,
-//         formatTooltipY: personify,
-//     },
-//     axisOptions: {
-//         // xAxisMode: 'tick',
-//         // yAxisMode: 'tick',
-//         truncateLegends: true
-//     }
-// });
-
 let chart = null;
 const makeChart = data => {
-    console.log("data", data);
-    if(chart) {
-        chart.data.labels = data.labels;
-        chart.data.datasets = data.datasets;
-        chart.update();
-        return;
-    }
+  console.log("data", data);
+  if (chart) {
+    chart.data.labels = data.labels;
+    chart.data.datasets = data.datasets;
+    chart.update();
+    return;
+  }
 
-    chart = new Chart("chart", {
-        type: 'line',
-        data: {
-            labels: data.labels,
-            datasets: data.datasets
-        },
-        options: {}
-    });
-    return chart;
+  chart = new Chart("chart", {
+    type: "line",
+    data: {
+      labels: data.labels,
+      datasets: data.datasets
+    },
+    options: {}
+  });
+  return chart;
 };
 
 const updateChart = data => [
-    () => {
-        makeChart(data);
-    }
+  () => {
+    makeChart(data);
+  }
 ];
 
 const toChartData = state => {
-    const [firstCountry] = state.selectedCountries;
-    return {
-        labels: firstCountry ? state.report[firstCountry].map(stats => stats.date) : [],
-        datasets: state.selectedCountries.map(name => {
-            const {r,g,b} = stringToRGB(name);
-            return {
-                label: name,
-                data: state.report[name].map(stats => stats.confirmed),
-                backgroundColor: [
-                        `rgba(${r}, ${g}, ${b}, 0.2)`
-                ],
-                borderColor: [
-                        `rgba(${r}, ${g}, ${b}, 1)`
-                ],
-            }
-        })
-    };
+  const [firstCountry] = state.selectedCountries;
+  return {
+    labels: firstCountry
+      ? state.report[firstCountry].map(stats => stats.date)
+      : [],
+    datasets: state.selectedCountries.map(name => {
+      const { r, g, b } = stringToRGB(name);
+      return {
+        label: name,
+        data: state.report[name].map(stats => stats.confirmed),
+        backgroundColor: [`rgba(${r}, ${g}, ${b}, 0.2)`],
+        borderColor: [`rgba(${r}, ${g}, ${b}, 1)`]
+      };
+    })
+  };
 };
 
 const html = htm.bind(h);
 
-const GotReport = (state, report) => ({...state, report});
+const GotReport = (state, report) => ({ ...state, report });
 const fetchReport = request({
-    url: "https://pomber.github.io/covid19/timeseries.json",
-    expect: "json",
-    action: GotReport
+  url: "https://pomber.github.io/covid19/timeseries.json",
+  expect: "json",
+  action: GotReport
 });
 const SelectCountry = (state, currentCountry) => ({
-    ...state,
-    currentCountry,
+  ...state,
+  currentCountry
 });
-const unique = list => [...new Set(list)];
-const AddCountry = state => {
+const AddCountryFromMap = currentCountry => state => {
     const newState = {
         ...state,
-        selectedCountries: unique([...state.selectedCountries, state.currentCountry]),
+        selectedCountries: unique([
+            ...state.selectedCountries,
+            currentCountry
+        ])
     };
     return [newState, [updateChart(toChartData(newState))]];
 };
-const RemoveCountry = country => state => {
+const RemoveCountryFromMap = currentCountry => state => {
     const newState = {
         ...state,
-        selectedCountries: state.selectedCountries.filter(c => c !== country)
+        selectedCountries: state.selectedCountries.filter(country => currentCountry !== country)
     };
     return [newState, [updateChart(toChartData(newState))]];
+};
+const unique = list => [...new Set(list)];
+const AddCountry = state => {
+  const newState = {
+    ...state,
+    selectedCountries: unique([
+      ...state.selectedCountries,
+      state.currentCountry
+    ])
+  };
+  return [newState, [updateChart(toChartData(newState))]];
+};
+const RemoveCountry = country => state => {
+  const newState = {
+    ...state,
+    selectedCountries: state.selectedCountries.filter(c => c !== country)
+  };
+  return [newState, [updateChart(toChartData(newState))]];
 };
 
 const countryNames = report => Object.keys(report).sort();
 
-const selectedOption = (selected, name) => selected === name ? html`<option selected value="${name}">${name}</option>` : html`<option value="${name}">${name}</option>`
+const selectedOption = (selected, name) =>
+  selected === name
+    ? html`
+        <option selected value="${name}">${name}</option>
+      `
+    : html`
+        <option value="${name}">${name}</option>
+      `;
+
+const isActive = state => country => state.selectedCountries.includes(country);
 
 app({
-    init: [{report: {}, currentCountry: "Poland", selectedCountries: []}, fetchReport],
-    view: state => console.log(state) ||
-        html`
+  init: [
+    { report: {}, currentCountry: "Poland", selectedCountries: [] },
+    fetchReport
+  ],
+  view: state =>
+    console.log(state) ||
+    html`
       <div>
-      <select oninput=${[SelectCountry, targetValue]} class="countries"> 
-            ${countryNames(state.report).map(name => selectedOption(state.currentCountry, name))}
-      </select>
-      <button onclick=${AddCountry}>Select</button>
-      <ul>
-      ${Array.from(state.selectedCountries).map(country => html`<li onclick=${RemoveCountry(country)}>${country} (x)</li>`)}
-</ul>
+        <select oninput=${[SelectCountry, targetValue]} class="countries">
+          ${countryNames(state.report).map(name =>
+            selectedOption(state.currentCountry, name)
+          )}
+        </select>
+        <button onclick=${AddCountry}>Select</button>
+        <ul>
+          ${Array.from(state.selectedCountries).map(
+            country =>
+              html`
+                <li onclick=${RemoveCountry(country)}>${country} (x)</li>
+              `
+          )}
+        </ul>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2000 1001">
+          ${Object.keys(countries).map(
+            country => html`
+              <path stroke="${isActive(state)(countries[country].name) ? stringToHex(countries[country].name) : ""}" 
+              onclick=${isActive(state)(countries[country].name) ? RemoveCountryFromMap(countries[country].name) : AddCountryFromMap(countries[country].name)} id="${country}" d="${countries[country].d}" />
+            `
+          )}
+        </svg>
       </div>
     `,
-    node: document.getElementById("control-panel")
+  node: document.getElementById("control-panel")
 });
--- stringToColor.js
+++ stringToColor.js
@@ -1,26 +1,27 @@
-function stringToHex(str) {
-    var hash = 0;
-    for (var i = 0; i < str.length; i++) {
-        hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    var colour = '#';
-    for (var i = 0; i < 3; i++) {
-        var value = (hash >> (i * 8)) & 0xFF;
-        colour += ('00' + value.toString(16)).substr(-2);
-    }
-    return colour;
+export function stringToHex(str) {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  var colour = "#";
+  for (var i = 0; i < 3; i++) {
+    var value = (hash >> (i * 8)) & 0xff;
+    colour += ("00" + value.toString(16)).substr(-2);
+  }
+  return colour;
 }
 
 function hexToRgb(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
         r: parseInt(result[1], 16),
         g: parseInt(result[2], 16),
         b: parseInt(result[3], 16)
-    } : null;
+      }
+    : null;
 }
 
-
 export function stringToRGB(str) {
-    return hexToRgb(stringToHex(str));
+  return hexToRgb(stringToHex(str));
 }
