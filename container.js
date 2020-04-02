7a0e71d37f51d5644ec8b4723b33a6372401fb61
kwasniew <kwasniewski.mateusz@gmail.com>
2020-04-02 13:10:51 +0200

    remove margin top

--- container.js
+++ container.js
@@ -1,7 +1,7 @@
 import { html } from "./html.js";
 
 export const Container = (props, children) => html`
-  <div class="${props.class || ""} mt-2 pt-2 pb-2">
+  <div class="${props.class || ""} pt-2 pb-2">
     <div class="container grid-md">
       ${children}
     </div>
